cada8a50ae68c435a51ea4757cf9bd67317aad3f
slpbaby <yan@yan-K53SC.(none)>
2014-06-06 10:58:38 +0800

    Fixed bug in lcd_init

--- platform/stm32f4/lcd.c
+++ platform/stm32f4/lcd.c
@@ -91,108 +91,110 @@ void __USER_TEXT lcd_write_data(uint8_t value)
 
 void __USER_TEXT lcd_poweron(void)
 {
-	uint8_t i;
-	lcd_write_cmd(0xca);
-	lcd_write_data(0xc3);
+	uint32_t i;
+	lcd_write_cmd(0xCA);
+	lcd_write_data(0xC3);
 	lcd_write_data(0x08);
 	lcd_write_data(0x50);
-	lcd_write_cmd(0xcf); //LCD_POWERB
+	lcd_write_cmd(0xCF); //LCD_POWERB
 	lcd_write_data(0x00);
-	lcd_write_data(0xc1);
+	lcd_write_data(0xC1);
 	lcd_write_data(0x30);
-	lcd_write_cmd(0xed); //LCD_POWER_SEQ
+	lcd_write_cmd(0xED); //LCD_POWER_SEQ
 	lcd_write_data(0x64);
 	lcd_write_data(0x03);
 	lcd_write_data(0x12);
 	lcd_write_data(0x81);
-	lcd_write_cmd(0xe8); //LCD_DTCA
+	lcd_write_cmd(0xE8); //LCD_DTCA
 	lcd_write_data(0x85);
 	lcd_write_data(0x00);
 	lcd_write_data(0x78);
-	lcd_write_cmd(0xcb); //LCD_POWERA
+	lcd_write_cmd(0xCB); //LCD_POWERA
 	lcd_write_data(0x39);
-	lcd_write_data(0x2c);
+	lcd_write_data(0x2C);
 	lcd_write_data(0x00);
 	lcd_write_data(0x34);
 	lcd_write_data(0x02);
-	lcd_write_cmd(0xf7); //LCD_PRC
+	lcd_write_cmd(0xF7); //LCD_PRC
 	lcd_write_data(0x20);
-	lcd_write_cmd(0xea); //LCD_DTCB
+	lcd_write_cmd(0xEA); //LCD_DTCB
 	lcd_write_data(0x00);
 	lcd_write_data(0x00);
-	lcd_write_cmd(0xb1); //LCD_FRC
+	lcd_write_cmd(0xB1); //LCD_FRC
 	lcd_write_data(0x00);
-	lcd_write_data(0x1b);
-	lcd_write_cmd(0xb6); //LCD_DFC
-	lcd_write_data(0x0a);
-	lcd_write_data(0xa2);
-	lcd_write_cmd(0xc1); //LCD_Power2
+	lcd_write_data(0x1B);
+	lcd_write_cmd(0xB6); //LCD_DFC
+	lcd_write_data(0x0A);
+	lcd_write_data(0xA2);
+	lcd_write_cmd(0xC0); //LCD_Power1
 	lcd_write_data(0x10);
-	lcd_write_cmd(0xc5); //LCD_VCOM1
+	lcd_write_cmd(0xC1); //LCD_Power2
+	lcd_write_data(0x10);
+	lcd_write_cmd(0xC5); //LCD_VCOM1
 	lcd_write_data(0x45);
 	lcd_write_data(0x15);
-	lcd_write_cmd(0xc7); //LCD_VCOM2
+	lcd_write_cmd(0xC7); //LCD_VCOM2
 	lcd_write_data(0x90);
 	lcd_write_cmd(0x36); //LCD_MAC
-	lcd_write_data(0xc8);
-	lcd_write_cmd(0xf2); //LCD_3GAMMA_EN
+	lcd_write_data(0xC8);
+	lcd_write_cmd(0xF2); //LCD_3GAMMA_EN
     lcd_write_data(0x00);
-    lcd_write_cmd(0xb0); //LCD_RGB_INTERFACE
-	lcd_write_data(0xc2);
-	lcd_write_cmd(0xb6); //LCD_DFC
-	lcd_write_data(0x0a);
-	lcd_write_data(0xa7);
+    lcd_write_cmd(0xB0); //LCD_RGB_INTERFACE
+	lcd_write_data(0xC2);
+	lcd_write_cmd(0xB6); //LCD_DFC
+	lcd_write_data(0x0A);
+	lcd_write_data(0xA7);
 	lcd_write_data(0x27);
 	lcd_write_data(0x04);
 
 	/* colomn address set */
-	lcd_write_cmd(0x2a); //LCD_COLUMN_ADDR
+	lcd_write_cmd(0x2A); //LCD_COLUMN_ADDR
 	lcd_write_data(0x00);
 	lcd_write_data(0x00);
 	lcd_write_data(0x00);
-	lcd_write_data(0xef);
+	lcd_write_data(0xEF);
 	/* Page Address Set */
-	lcd_write_cmd(0x2b); //LCD_PAGE_ADDR
+	lcd_write_cmd(0x2B); //LCD_PAGE_ADDR
 	lcd_write_data(0x00);
 	lcd_write_data(0x00);
 	lcd_write_data(0x01);
-	lcd_write_data(0x3f);
-	lcd_write_cmd(0xf6); //LCD_INTERFACE
+	lcd_write_data(0x3F);
+	lcd_write_cmd(0xF6); //LCD_INTERFACE
 	lcd_write_data(0x01);
 	lcd_write_data(0x00);
 	lcd_write_data(0x06);
 
-	lcd_write_cmd(0x2c); //LCD_GRAM
+	lcd_write_cmd(0x2C); //LCD_GRAM
 	//L4_Sleep(200);
 	/* L4_Sleep unable to be used at here, so I use while temporary */
 	/* TODO: A proper delay */
   	i = 0;
-	while (i < 200)
+	while (i < 400)
 		i++;
 
 	lcd_write_cmd(0x26); //LCD_GAMMA
 	lcd_write_data(0x01);
 	
-	lcd_write_cmd(0xe0); //LCD_PGAMMA
-	lcd_write_data(0x0f);
+	lcd_write_cmd(0xE0); //LCD_PGAMMA
+	lcd_write_data(0x0F);
 	lcd_write_data(0x29);
 	lcd_write_data(0x24);
-	lcd_write_data(0x0c);
-	lcd_write_data(0x0e);
+	lcd_write_data(0x0C);
+	lcd_write_data(0x0E);
 	lcd_write_data(0x09);
-	lcd_write_data(0x4e);
+	lcd_write_data(0x4E);
 	lcd_write_data(0x78);
-	lcd_write_data(0x3c);
+	lcd_write_data(0x3C);
 	lcd_write_data(0x09);
 	lcd_write_data(0x13);
 	lcd_write_data(0x05);
 	lcd_write_data(0x17);
 	lcd_write_data(0x11);
 	lcd_write_data(0x00);
-	lcd_write_cmd(0xe1); //LCD_NGAMMA
+	lcd_write_cmd(0xE1); //LCD_NGAMMA
 	lcd_write_data(0x00);
 	lcd_write_data(0x16);
-	lcd_write_data(0x1b);
+	lcd_write_data(0x1B);
 	lcd_write_data(0x04);
 	lcd_write_data(0x11);
 	lcd_write_data(0x07);
@@ -200,38 +202,54 @@ void __USER_TEXT lcd_poweron(void)
 	lcd_write_data(0x33);
 	lcd_write_data(0x42);
 	lcd_write_data(0x05);
-	lcd_write_data(0x0c);
-	lcd_write_data(0x0a);
+	lcd_write_data(0x0C);
+	lcd_write_data(0x0A);
 	lcd_write_data(0x28);
-	lcd_write_data(0x2f);
-	lcd_write_data(0x0f);
+	lcd_write_data(0x2F);
+	lcd_write_data(0x0F);
+
 	lcd_write_cmd(0x11); //LCD_SLEEP_OUT
 	//L4_Sleep(200);
 	/* L4_Sleep unable to be used at here, so I use while temporary */
 	/* TODO: A proper delay */
   	i = 0;
-	while (i < 200)
+	while (i < 400)
 		i++;
 	lcd_write_cmd(0x29); //LCD_DISPLAY_ON
 	/* GRAM start writing */
-	lcd_write_cmd(0x2c); //LCD_GRAM
+	lcd_write_cmd(0x2C); //LCD_GRAM
+}
+
+void __USER_TEXT lcd_display_off(void)
+{
+	lcd_write_cmd(0x28);
 }
 
 void __USER_TEXT lcd_spi_config(void)
 {
 	struct spi_cfg spi_init;
+	struct gpio_cfg gpio_init;
 	/* Enable clock */
 	RCC_AHB1PeriphClockCmd(LCD_SPI_SCK_GPIO_CLK | LCD_SPI_MISO_GPIO_CLK | LCD_SPI_MOSI_GPIO_CLK, 1);
 
 	RCC_APB2PeriphClockCmd(LCD_SPI_CLK, 1);
 
-	gpio_config_output(LCD_SPI_SCK_GPIO_PORT, LCD_SPI_SCK_PIN, GPIO_PUPDR_DOWN, GPIO_OSPEEDR_25M);
-	gpio_config_output(LCD_SPI_MISO_GPIO_PORT, LCD_SPI_MISO_PIN, GPIO_PUPDR_DOWN, GPIO_OSPEEDR_25M);
-	gpio_config_output(LCD_SPI_MOSI_GPIO_PORT, LCD_SPI_MOSI_PIN, GPIO_PUPDR_DOWN, GPIO_OSPEEDR_25M);
+	gpio_init.port = LCD_SPI_SCK_GPIO_PORT;
+	gpio_init.pin = LCD_SPI_SCK_PIN;
+	gpio_init.speed = GPIO_OSPEEDR_25M;
+	gpio_init.pupd = GPIO_PUPDR_DOWN;
+	gpio_init.type = GPIO_MODER_ALT;
+	gpio_init.func = af_spi5;
+	gpio_init.o_type = GPIO_OTYPER_PP;
+	gpio_config(&gpio_init);
 
-	gpio_pinaf_config(LCD_SPI_SCK_GPIO_PORT, LCD_SPI_SCK_PIN, af_spi5);
-	gpio_pinaf_config(LCD_SPI_MISO_GPIO_PORT, LCD_SPI_MISO_PIN, af_spi5);
-	gpio_pinaf_config(LCD_SPI_MOSI_GPIO_PORT, LCD_SPI_MOSI_PIN, af_spi5);
+	gpio_init.port = LCD_SPI_MISO_GPIO_PORT;
+	gpio_init.pin = LCD_SPI_MISO_PIN;
+	gpio_config(&gpio_init);
+
+	gpio_init.port = LCD_SPI_MOSI_GPIO_PORT;
+	gpio_init.pin = LCD_SPI_MOSI_PIN;
+	gpio_config(&gpio_init);
 
 	spi_i2s_reset(LCD_SPI);
 
@@ -271,8 +289,8 @@ void __USER_TEXT lcd_ctrllines_write(uint8_t port, uint8_t pin, uint8_t reset)
 void __USER_TEXT lcd_ctrllines_init(void)
 {
 	RCC_AHB1PeriphClockCmd(LCD_NCS_GPIO_CLK | LCD_WRX_GPIO_CLK, 1);
+	gpio_config_output(LCD_NCS_GPIO_PORT, LCD_NCS_PIN, GPIO_PUPDR_NONE, GPIO_OSPEEDR_50M);
 	gpio_config_output(LCD_WRX_GPIO_PORT, LCD_WRX_PIN, GPIO_PUPDR_NONE, GPIO_OSPEEDR_50M);
-	gpio_config_output(LCD_WRX_GPIO_PORT, LCD_NCS_PIN, GPIO_PUPDR_NONE, GPIO_OSPEEDR_50M);
 
 	lcd_ctrllines_write(LCD_NCS_GPIO_PORT, LCD_NCS_PIN, 1);
 }
